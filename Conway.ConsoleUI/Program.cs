5bc959f52e508b3e97bb624fbd9bc73ad4b442a6
Jeremy Clark <jeremy@jeremybytes.com>
2015-01-02 20:42:02 -0800

    Added Console UI

---
+++ Conway.ConsoleUI/Program.cs
@@ -0,0 +1,64 @@
+﻿using Conway.Library;
+using System;
+using System.Text;
+
+namespace Conway.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid = new LifeGrid(25, 65);
+            grid.Randomize();
+
+            ShowGrid(grid.CurrentState);
+
+            while (Console.ReadLine() != "q")
+            {
+                grid.UpdateState();
+                ShowGrid(grid.CurrentState);
+            }
+        }
+
+        private static void ShowGrid(CellState[,] currentState)
+        {
+            Console.Clear();
+            int x = 0;
+            int rowLength = currentState.GetUpperBound(1) + 1;
+
+            var output = new StringBuilder();
+
+            foreach (var state in currentState)
+            {
+                output.Append(state == CellState.Alive ? "O" : "·");
+                x++;
+                if (x >= rowLength)
+                {
+                    x = 0;
+                    output.AppendLine();
+                }
+            }
+            Console.Write(output.ToString());
+        }
+
+        // THIS WAS A SLOW DISPLAY ON LARGE GRIDS
+        //private static void ShowGrid(CellState[,] currentState)
+        //{
+        //    Console.Clear();
+        //    int x = 0;
+        //    int rowLength = currentState.GetUpperBound(1) + 1;
+
+        //    foreach (var state in currentState)
+        //    {
+        //        var output = state == CellState.Alive ? "O" : "·";
+        //        Console.Write(output);
+        //        x++;
+        //        if (x >= rowLength)
+        //        {
+        //            x = 0;
+        //            Console.WriteLine();
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Conway.Library/LifeGrid.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Conway.Library
+{
+    public class LifeGrid
+    {
+        int gridHeight;
+        int gridWidth;
+
+        public CellState[,] CurrentState;
+        private CellState[,] nextState;
+
+        public LifeGrid(int height, int width)
+        {
+            gridHeight = height;
+            gridWidth = width;
+
+            CurrentState = new CellState[gridHeight, gridWidth];
+            nextState = new CellState[gridHeight, gridWidth];
+
+            for (int i = 0; i < gridHeight; i++)
+                for (int j = 0; j < gridWidth; j++)
+                {
+                    CurrentState[i, j] = CellState.Dead;
+                }
+        }
+
+        public void UpdateState()
+        {
+            for (int i = 0; i < gridHeight; i++)
+                for (int j = 0; j < gridWidth; j++)
+                {
+                    var liveNeighbors = GetLiveNeighbors(i, j);
+                    nextState[i, j] = 
+                        LifeRules.GetNewState(CurrentState[i, j], liveNeighbors);
+                }
+
+            CurrentState = nextState;
+            nextState = new CellState[gridHeight, gridWidth];
+        }
+
+        public void Randomize()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < gridHeight; i++)
+                for (int j = 0; j < gridWidth; j++)
+                {
+                    var next = random.Next(2);
+                    var newState = next < 1 ? CellState.Dead : CellState.Alive;
+                    CurrentState[i, j] = newState;
+                }
+        }
+
+        private int GetLiveNeighbors(int positionX, int positionY)
+        {
+            int liveNeighbors = 0;
+            for(int i = -1; i <= 1; i++)
+                for(int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+
+                    int neighborX = positionX + i;
+                    int neighborY = positionY + j;
+
+                    if (neighborX >= 0 && neighborX < gridHeight &&
+                        neighborY >= 0 && neighborY < gridWidth)
+                    {
+                        if (CurrentState[neighborX, neighborY] == CellState.Alive)
+                            liveNeighbors++;
+                    }
+                }
+
+            return liveNeighbors;
+        }
+    }
+}
